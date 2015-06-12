385da5cfbda8bc11c2bfb45403866bbd1684f255
Leonardo Donelli <learts92@gmail.com>
2015-06-12 21:35:26 +0200

    lunch-tracker: correct redis keys, send two separate responses
    Include the chat id in the redis key to make the plugin work on
    multiple channels simultaneously, insert missing ':' in the key,
    if a message contains both the lunch description and a vote
    send the two responses in two different messages by using the
    send_large_msg functionn instead of returning the response.

--- lunch-tracker.lua
+++ lunch-tracker.lua
@@ -2,32 +2,31 @@ local SAVE_LUNCH_PATTERN = ".*[oO]ggi abbiamo mangiato ([^%.]*)"
 local CAST_VOTE_PATTERN = ".*[vV]oto (%d+).*"
 
 function cast_vote(lunch, vote, user)
-   redis:hset(lunch..'votes', user.id, vote)
+   redis:hset(lunch .. ':votes', user.id, vote)
    return "Voto registrato per " .. user.first_name .. ": " .. vote .. "."
 end
 
 function set_lunch(lunch, lunch_description)
-   redis:set(lunch..'description', lunch_description)
-   return "Ah, quindi oggi avete mangiato "..lunch_description..". Beati voi,"..
-      " io mi nutro solamente di corrente elettrica del server su cui giro.\n"
+   redis:set(lunch .. ':description', lunch_description)
+   return "Ah, quindi oggi avete mangiato " .. lunch_description ..
+      ". Beati voi, io mi nutro solamente di corrente elettrica del" ..
+      " server su cui giro.\n"
 end
 
 function run(msg, matches)
    local today = os.date('%Y-%m-%d')
-   local key = 'lunch:'..today
+   local key = 'chat:' .. msg.to.id .. ':lunch:' .. today
 
-   message = ""
    lunch_description = string.match(msg.text, SAVE_LUNCH_PATTERN)
-   vote = string.match(msg.text, CAST_VOTE_PATTERN)
    if lunch_description then
-      message = message .. set_lunch(key, lunch_description)
-   end
-   if vote then
-      message = message .. cast_vote(key, vote, msg.from)
+      local response = set_lunch(key, lunch_description)
+      send_large_msg(get_receiver(msg), response)
    end
 
-   if string.len(message) > 0 then
-      return message
+   vote = string.match(msg.text, CAST_VOTE_PATTERN)
+   if vote then
+      local response = cast_vote(key, vote, msg.from)
+      send_large_msg(get_receiver(msg), response)
    end
 end
 
