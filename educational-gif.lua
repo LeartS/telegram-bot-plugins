fd6d6d85a97e2125d90774c6f4e4285f44d62141
Leonardo Donelli <learts92@gmail.com>
2015-06-12 21:11:33 +0200

    New plugin: educational-gif
    Sends a random submission from reddit.com/r/educationalgifs.
    Allows you to set the max gif size and minimum score of the
    submission, if one doesn't match those conditions it will
    try another one.

---
+++ educational-gif.lua
@@ -0,0 +1,50 @@
+local MIN_SUBMISSION_SCORE = 5 -- If less, ignore and try another submission
+local MAX_GIF_SIZE = 10485760 -- In bytes. If bigger, try another submission
+
+function get_random_submission()
+   local r, s = http.request(
+      "http://api.reddit.com/r/educationalgifs/random.json?t=" .. os.time()
+   )
+   return json:decode(r)[1]['data']['children'][1]['data']
+end
+
+-- Adapt some known problematic urls to make them work
+function fix_url(url)
+   if url:match(".*imgur.com.*%.gifv") then
+      -- the same url .gif should work
+      return url:sub(1, -2)
+   elseif url:match("^.*gfycat.com/(%a+)$") then
+      local gfyid = string.match(url, "^.*gfycat.com/(%a+)$")
+      return 'http://giant.gfycat.com/' .. gfyid ..'.gif'
+   end
+   return url
+end
+
+function run(msg, matches)
+   local submission, url
+   repeat
+      repeat
+         submission = get_random_submission()
+      until submission['score'] >= MIN_SUBMISSION_SCORE
+      url = fix_url(submission['url'])
+      print(submission['url'])
+      print(url)
+      -- Download the gif initally to check its size.
+      -- Wasteful as it will redownload it later inside send_document_from_url
+      -- But I couldn't find any better way to check for the gif size without
+      -- downloading it, and we really want to avoid sending 70MB gifs.
+      -- Downloading them multiple times on the server is not much of a problem
+      local gif, status = http.request(url)
+      print(#gif)
+   until status == 200 and gif:len() <= MAX_GIF_SIZE
+
+   send_large_msg(get_receiver(msg), submission['title'])
+   send_document_from_url(get_receiver(msg), url)
+end
+
+return {
+	description = "Educationl Gif",
+	usage = "!edugif",
+	patterns = { "^!edugif$" },
+	run = run
+}
