8a3c87290a78e2c17f7ea25573a785224d2589e0
Leonardo Donelli <learts92@gmail.com>
2015-06-24 09:35:16 +0200

    raccolta-differenziata: avvisa il giorno prima
    Fix bug critico: marvin avvisava il giorno stesso, quando ormai
    è troppo tardi, di quello che c'è da portare fuori. Ora avvisa
    la sera del giorno prima.

--- raccolta-differenziata.lua
+++ raccolta-differenziata.lua
@@ -1,12 +1,12 @@
 local CHANNEL = 'chat#id13820496'
 local DAYS = {
-   nil, -- Sunday
-   'l\' indifferenziata', -- Monday
-   'umido e plastica', -- Tuesday
-   'la carta', -- Wednesday
-   'umido ed indifferenziata', -- Thursday
-   'la plastica', -- Friday
-   'umido e carta' -- Saturday
+   'l\' indifferenziata', -- Sunday
+   'umido e plastica', -- Monday
+   'la carta', -- Tuesday
+   'umido ed indifferenziata', -- Wednesday
+   'la plastica', -- Thursday
+   'umido e carta', -- Friday
+   nil, -- Saturday
 }
 
 local function cron()
@@ -16,8 +16,8 @@ local function cron()
    if now.hour == 17 and now.min > 40 and now.min < 46 then
       if DAYS[now.wday] ~= nil then
          local m = 'Ragazzi ricordatevi che oggi bisogna portare fuori '
-         if now.wday == 7 then
-            -- Special message for saturday
+         if now.wday == 1 then
+            -- Special message for Sunday
             m = 'Se per caso qualcuno si trova in ufficio, ci sarebbe da' ..
                ' portare fuori '
          end
